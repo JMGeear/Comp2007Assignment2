9dafcab69071b1bc36dceda59a446e18cd7178e0
Jeff Geear <jmgeear@gmail.com>
2015-07-17 12:40:53 -0400

    view bible

---
+++ Comp2007Assignment2/bibleMenu.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="bibleMenu.aspx.cs" Inherits="Comp2007Assignment2.bibleMenu" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Booklist</h1>
+
+    <asp:GridView ID="grdBible" runat="server" CssClass="table table-striped"
+        AutoGenerateColumns="false" DataKeyNames="ID">
+        <Columns>        
+            <asp:BoundField DataField="Book" HeaderText="Book" />
+            <asp:BoundField DataField="Chapter" HeaderText="Chapter" DataFormatString="{0}" />
+            <asp:BoundField DataField="Verse" HeaderText="Verse" DataFormatString="{0}" />
+            <asp:BoundField DataField="VerseText" HeaderText="Verse" />
+        </Columns>
+
+    </asp:GridView>
+</asp:Content>
---
+++ Comp2007Assignment2/bibleMenu.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//reference our entity framework models
+using Comp2007Assignment2.Models;
+using System.Web.ModelBinding;
+
+namespace Comp2007Assignment2
+{
+    public partial class bibleMenu : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //fill the grid
+            if (!IsPostBack)
+            {
+                GetBible();
+            }
+        }
+        protected void GetBible()
+        {
+
+            //connect using our connection string from web.config and EF context class
+            using (DefaultConnection conn = new DefaultConnection())
+            {
+                //use link to query the Departments model
+                var bible = from b in conn.BibleBasicEnglishes
+                           select b;
+
+                //bind the query result to the gridview
+                grdBible.DataSource = bible.ToList();
+                grdBible.DataBind();
+            }
+        }
+
+
+    }
+}
