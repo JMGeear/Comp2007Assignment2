974b1895c969c666be72fbec25ee76d81bcb5338
Benjamin Chapman <benjaminchapman@rogers.com>
2015-07-20 12:57:12 -0400

    Added notes list page. Moved menu to admin. Adjusted links on site master

---
+++ Comp2007Assignment2/admin/notes.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="notes.aspx.cs" Inherits="Comp2007Assignment2.admin.notes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <a href="notes.aspx">Add Note</a>
+    <asp:GridView ID="grdNotes" runat="server">
+        <Columns>
+            <asp:BoundField DataField="Title" HeaderText="Title"/>
+
+        </Columns>
+    </asp:GridView>
+</asp:Content>
--- Comp2007Assignment2/register.aspx.cs
+++ Comp2007Assignment2/register.aspx.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.Entity.Validation;
+
+
+using Comp2007Assignment2.Models;
+using System.Web.ModelBinding;
 
 namespace Comp2007Assignment2
 {
@@ -16,7 +21,21 @@ namespace Comp2007Assignment2
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
+            using (DefaultConnection db = new DefaultConnection())
+            {
 
+                    user u = new user();
+
+                    u.fName = txtFName.Text;
+                    u.lName = txtLName.Text;
+                    u.username = txtUsername.Text;
+                    u.password = txtPassword.Text;
+
+                    db.users.Add(u);
+                    db.SaveChanges();
+
+                
+            } 
         }
 	}
 }
