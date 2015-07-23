1dbdeca902baf63ad30fef2180e05a06367cd698
Jeff Geear <jmgeear@gmail.com>
2015-07-22 23:15:48 -0400

    note page

--- Comp2007Assignment2/Site.Master.designer.cs
+++ Comp2007Assignment2/Site.Master.designer.cs
@@ -49,6 +49,15 @@ namespace Comp2007Assignment2 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// asm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager asm;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
